70fcc50ee1a14301a331c37f07aff527bcbec598
alvaro <alvaro@micorreo.com>
2026-01-07 09:18:07 +0100

    feat(VetLine/sprint#1)Create Cliente y TarjetaCredito Models

---
+++ src/main/java/org/example/bankback/domain/models/Cliente.java
@@ -0,0 +1,11 @@
+package org.example.bankback.domain.models;
+
+public class Cliente {
+    String username;
+    String password;
+    String nombre;
+    String apellido1;
+    String apellido2;
+    String dni;
+    String api_token;
+}
---
+++ src/main/java/org/example/bankback/domain/models/TarjetaCredito.java
@@ -0,0 +1,8 @@
+package org.example.bankback.domain.models;
+
+public class TarjetaCredito {
+    int numeroTarjeta;
+    String fechaCaducidad;
+    String cvc;
+    String nombreCompleto;
+}
